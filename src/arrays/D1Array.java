8e5edc7a4d740ffa800c8264bc252017004e8867
Aditya Kalambe <adityakalambe20@gmail.com>
2024-04-01 23:42:10 +0530

    Final

---
+++ src/arrays/D1Array.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class D1Array {
+    public static void main(String[] args) {
+        int[] numberArray = {1,2,3,4,5,6,7};
+        for (int j : numberArray) {
+            System.out.println(j);
+        }
+    }
+}
